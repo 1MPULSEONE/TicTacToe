f3064f46e16848e44cd0fbe8a2e94df5aebf030f
1MPULSEONE <parkhomenko2049@gmail.com>
2020-07-12 18:33:27 +0600

    Main.java modified. Parts of the code are merged,some bags fixed.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,13 @@
-package tictactoe;
+package com.company;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter cells: ");
-        String input = scanner.nextLine();
-        input = input.replace('_',' ');
-        char[] symbols = input.toCharArray();
+        char[][] symbols = new char[3][3];
         int counter = 0;
+        char userChar;
+        int counterChar = 0;
         int cordinateX = 0;
         int cordinateY = 0;
         int counterX = 0;
@@ -26,15 +25,19 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             System.out.print("| ");
             for (int j = 0; j < 3; j++) {
-                output[i][j] = symbols[counter];
-                counter++;
+                output[i][j] = ' ';
                 System.out.print(output[i][j] + " ");
             }
             System.out.println("|");
         }
         System.out.println("---------");
-        counter = 0;
         while (loop) {
+
+            if (counterChar % 2 == 0) {
+                userChar = 'X';
+            } else {
+                userChar = 'O';
+            }
             System.out.print("Enter the coordinates: ");
             try {
                 String coordinatesString = scanner.nextLine();
@@ -60,106 +63,47 @@ public class Main {
             for (int i = 0; i < 3; i++) {
                 System.out.print("| ");
                 for (int j = 0; j < 3; j++) {
-                    if ((output[cordinateX][cordinateY] == ' ' || output[cordinateX][cordinateY] == '_') && counter == 0 ) {
-                        output[cordinateX][cordinateY] = 'X';
-                        counter++;
-                    }
+                    output[cordinateX][cordinateY] = userChar;
+                    sum += output[i][j];
                     System.out.print(output[i][j] + " ");
+                    sum = 0;
                 }
                 System.out.println("|");
             }
             System.out.println("---------");
-            loop = false;
+            counterChar++;
         }
-           /* for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    sum += output[i][j];
-                    if (output[i][j] == 'X') {
-                        counterX++;
-                    }
-                    if (output[i][j] == 'O') {
-                        counterO++;
-                    }
-                    // left-right diagonal
-                    if (output[0][0] + output[1][1] + output[2][2] == 264) {
-                        System.out.println("X wins");
-                        loop = false;
-                        loopTwo = false;
-                        i = 2;
-                        j = 2;
-                    } else if (output[0][0] + output[1][1] + output[2][2] == 237) {
-                        System.out.println("O wins");
-                        loop = false;
-                        loopTwo = false;
-                        i = 2;
-                        j = 2;
-                    }
-                    // right-left diagonal
-                    if (output[0][2] + output[1][1] + output[2][0] == 264) {
-                        System.out.println("X wins");
-                        loop = false;
-                        loopTwo = false;
-                        i = 2;
-                        j = 2;
-                    } else if (output[0][2] + output[1][1] + output[2][0] == 237) {
-                        System.out.println("O wins");
-                        loop = false;
-                        loopTwo = false;
-                        i = 2;
-                        j = 2;
-                    }
-                    // up-down
-                    if (output[0][j] + output[1][j] + output[2][j] == 264) {
-                        xWins = true;
-                    }
-                    if (output[0][j] + output[1][j] + output[2][j] == 237) {
-                        oWins = true;
-                    }
-                }
-                // row
-                if (sum == 264) {
-                    System.out.println("X wins");
-                    loop = false;
-                    loopTwo = false;
-                    break;
-                } else if (sum == 237) {
-                    System.out.println("O wins");
-                    loop = false;
-                    loopTwo = false;
-                    break;
-                }
-                sum = 0;
-                if (i == 2) {
-                    loop = false;
-                }
-            }
-        }
-        while (loopTwo) {
-            //impossible part
-            if (Math.abs(counterX - counterO) > 1 || (xWins && oWins)) {
-                System.out.println("Impossible");
-                break;
-            }
-            // checked x and o
-            if (xWins) {
-                System.out.println("X wins");
-                break;
-            }
-            if (oWins) {
-                System.out.println("O wins");
-                break;
-            }
-            //check draw
-            if (counterX + counterO == 9) {
-                System.out.println("Draw");
-                break;
-            }
-            //check game not finished
-            if (counterX == 3 && counterO == 3) {
-                System.out.println("Game not finished");
-                break;
-            }
-        }
-            */
+/* for (int i = 0; i < 3; i++) {
+for (int j = 0; j < 3; j++) {
+}
+}
+}
+while (loopTwo) {
+//impossible part
+if (Math.abs(counterX - counterO) > 1 || (xWins && oWins)) {
+System.out.println("Impossible");
+break;
+}
+// checked x and o
+if (xWins) {
+System.out.println("X wins");
+break;
+}
+if (oWins) {
+System.out.println("O wins");
+break;
+}
+//check draw
+if (counterX + counterO == 9) {
+System.out.println("Draw");
+break;
+}
+//check game not finished
+if (counterX == 3 && counterO == 3) {
+System.out.println("Game not finished");
+break;
+}
+}
+*/
     }
 }
